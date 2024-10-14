c063679e0cc3a95bdd1eda9a2d31444dbe7b88b8
DevCarps <1.johncarpila@gmail.com>
2024-10-14 23:25:46 +0800

    edited for telegram

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <main class="p-4">
-    <div v-if="userData">
-      <h1 class="text-2xl font-bold mb-4">User Data</h1>
-      <ul>
-        <li>ID: {{ userData.id }}</li>
-        <li>First Name: {{ userData.first_name }}</li>
-        <li>Last Name: {{ userData.last_name || 'N/A' }}</li>
-        <li>Username: {{ userData.username || 'N/A' }}</li>
-        <li>Language Code: {{ userData.language_code }}</li>
-        <li>Is Premium: {{ userData.is_premium ? 'Yes' : 'No' }}</li>
-      </ul>
-    </div>
-    <div v-else>Loading...</div>
-  </main>
+  <div>
+    <h1>Welcome to Telegram</h1>
+    <main class="p-4">
+      <div v-if="userData">
+        <h2 class="text-2xl font-bold mb-4">User Data</h2>
+        <ul>
+          <li>ID: {{ userData.id }}</li>
+          <li>First Name: {{ userData.first_name }}</li>
+          <li>Last Name: {{ userData.last_name || 'N/A' }}</li>
+          <li>Username: {{ userData.username || 'N/A' }}</li>
+          <li>Language Code: {{ userData.language_code }}</li>
+          <li>Is Premium: {{ userData.is_premium ? 'Yes' : 'No' }}</li>
+        </ul>
+      </div>
+      <div v-else>Loading...</div>
+    </main>
+  </div>
 </template>
 
 <script lang="ts">
@@ -33,8 +36,10 @@ export default defineComponent({
     const userData = ref<UserData | null>(null);
 
     onMounted(() => {
-      if (WebApp.initDataUnsafe.user) {
+      if (WebApp.initDataUnsafe && WebApp.initDataUnsafe.user) {
         userData.value = WebApp.initDataUnsafe.user as UserData;
+      } else {
+        console.warn('User data is not available. Ensure the app is launched from Telegram.');
       }
     });
 
