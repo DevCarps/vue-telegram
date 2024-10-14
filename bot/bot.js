0b2b09077dd12aa1921c7b2fe11e618bf3bfb118
DevCarps <1.johncarpila@gmail.com>
2024-10-15 00:16:42 +0800

    bot in telegram

--- bot/bot.js
+++ bot/bot.js
@@ -3,9 +3,11 @@ const { Telegraf } = require('telegraf');
 // Replace 'YOUR_BOT_TOKEN' with the token you got from BotFather
 const bot = new Telegraf('7623273613:AAFmaxOgiur7E1uM70cuQa08LPqA-9FhVUQ');
 
+const web_link = 'https://vue-telegram-sample.netlify.app/'
+
 bot.start((ctx) => {
   // Send a welcome message with the link to your web app
-  ctx.reply(`Welcome! Click here to open the app: https://vue-telegram-sample.netlify.app/`);
+  ctx.reply("Welcome!", {reply_markup: {keyboard:[[{text: "Web app", web_app: { url:web_link } }]]}} );
 });
 
 // Start the bot
