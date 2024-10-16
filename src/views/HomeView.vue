960d12e968873a708c6e9c56cecdd49e264f62c0
DevCarps <1.johncarpila@gmail.com>
2024-10-16 14:10:06 +0800

    back to original

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,61 +1,20 @@
 <template>
-  <div class="flex items-center justify-center rounded-lg">
-    <div class="wrapper bg-white bg-opacity-60 p-10 rounded-lg border shadow-lg">
-      <form @submit.prevent="">
-        <h1 class="text-2xl font-bold mb-6 text-center text-gray-800">Login</h1>
-
-        
-
-        <div class="input-box relative mb-4">
-          <input
-            type="text"
-             
-            placeholder="Username"  
-            required
-            class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-gray-500"
-          />
-          <i class="bx bxs-user absolute right-3 top-3 text-gray-400"></i>
-        </div>
-
-        <div class="input-box relative mb-4">
-          <input
-            type="password"
-            
-            placeholder="Password"
-            required
-            class="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-gray-500"
-          />
-          <i class="bx bxs-lock-alt absolute right-3 top-3 text-gray-400"></i>
-        </div>
-
-        <div class="flex justify-between mb-4 text-gray-600">
-          <label class="flex items-center">
-            <input type="checkbox"  class="mr-2 accent-gray-600" />
-            Remember me
-          </label>
-          <a href="#" class="hover:underline ml-4">Forgot password?</a>
-        </div>
-
-        <button
-          type="submit"
-          class="w-full text-gray-700 bg-slate-50 hover:text-white border border-gray-700 hover:bg-gray-400 px-4 py-2 rounded-lg transition duration-300 ease-in-out"
-            
-          >
-          <span >
-            <i class="bx bx-loader-alt loading"></i> Loading...
-          </span>
-          <span >
-            Login
-          </span>
-        </button>
-
-        <div class="mt-4 text-center text-gray-600">
-          <p>Don't have an account? 
-            <router-link to="/register" class="text-gray-600 hover:underline">Register</router-link>
-          </p>
-        </div>
-      </form>
-    </div>
+  <div class="text-white">
+    <h1>Welcome to Telegram</h1>
+    <main class="p-4">
+      <div v-if="userData">
+        <h2 class="text-2xl font-bold mb-4">User Data</h2>
+        <ul>
+          <li>ID: {{ userData.id }}</li>
+          <li>First Name: {{ userData.first_name }}</li>
+          <li>Last Name: {{ userData.last_name || 'N/A' }}</li>
+          <li>Username: {{ userData.username || 'N/A' }}</li>
+          <li>Language Code: {{ userData.language_code }}</li>
+          <li>Is Premium: {{ userData.is_premium ? 'Yes' : 'No' }}</li>
+        </ul>
+      </div>
+      <div v-else>Loading...</div>
+    </main>
   </div>
 </template>
 
