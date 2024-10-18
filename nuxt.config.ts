9de0831bdd0dec8155dea4c017c45cf040222c59
DevCarps <1.johncarpila@gmail.com>
2024-10-18 19:51:58 +0800

    nuxt config and in useWebApp

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+export default defineNuxtConfig({
+    app: {
+      head: {
+        script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }],
+      },
+    },
+  })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,16 @@
-<template>
-  <div class="text-black">
-    <h1>Welcome to Telegram</h1>
-    <main class="p-4">
-      <div v-if="userData">
-        <h2 class="text-2xl font-bold mb-4">User Data</h2>
-        <ul>
-          <li>ID: {{ userData.id }}</li>
-          <li>First Name: {{ userData.first_name }}</li>
-          <li>Last Name: {{ userData.last_name || 'N/A' }}</li>
-          <li>Username: {{ userData.username || 'N/A' }}</li>
-          <li>Language Code: {{ userData.language_code }}</li>
-          <li>Is Premium: {{ userData.is_premium ? 'Yes' : 'No' }}</li>
-        </ul>
-      </div>
-      <div v-else>Loading...</div>
-    </main>
-  </div>
-</template>
+<script lang="ts" setup>
+import { MainButton, useWebAppPopup, useWebApp   } from 'vue-tg';
 
-<script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
-import WebApp from '@twa-dev/sdk';
+const { showAlert } = useWebAppPopup()
+const { initdata, initDataUnsafe } = useWebApp()
 
-interface UserData {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  language_code: string;
-  is_premium?: boolean;
-}
-
-export default defineComponent({
-  setup() {
-    const userData = ref<UserData | null>(null);
-
-    onMounted(() => {
-      // Check if the Telegram SDK is loaded
-      console.log("Telegram SDK:", window.Telegram); // Log the entire object
-
-       // Call WebApp.ready() to initialize the SDK
-       window.Telegram.WebApp.ready();
-
-        // Inspect initDataUnsafe
-      console.log("WebApp Data:", WebApp.initDataUnsafe);
-
-      // Detailed log of the initDataUnsafe structure
-      console.log("WebApp Data Structure:", JSON.stringify(WebApp.initDataUnsafe, null, 2));
-
-
-       // Check if user data is available
-      if (WebApp.initDataUnsafe && WebApp.initDataUnsafe.user) {
-        userData.value = WebApp.initDataUnsafe.user as UserData;
-        console.log("User data:", userData.value); // Log the user data
-      } else {
-        console.warn('User data is not available. Ensure the app is launched from Telegram.');
-      }
-    });
-
-    return { userData };
-  },
-});
 </script>
 
-<style scoped>
+<template>
+  <h2>User Data</h2>
+  <pre>{{ initdata }}</pre>
+  <pre>{{ initDataUnsafe }}</pre>
+  <MainButton text="Tap alert" @click="() => showAlert('Taena pano yun')" />
 
-</style>
+</template>
+
